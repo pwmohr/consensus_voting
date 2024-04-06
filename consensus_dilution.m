b011950723e507e850a0ddb48990792b3d9e9f61
Paul <pwmohr@gmail.com>
2024-04-06 13:35:13 -0700

    Spot check seems to be working.

--- consensus_dilution.m
+++ consensus_dilution.m
@@ -2,15 +2,8 @@
 epsilon = 0.01;
 numChoices = 100;
 participants = 5;
-votesPerPerson = 10;
+votesPerPerson = 100;
 deltaExitCriteria = 5;
-
-[c, d, n] = simulate_voting( numChoices, participants, votesPerPerson, epsilon, deltaExitCriteria);
-
-fprintf("Consensus, mean: %f\n", c);
-fprintf("Dilution, mean: %f\n", d);
-fprintf("Number of simulations: %d\n", n);
-
 
 function [cons_avg, dilu_avg, n] = simulate_voting(numChoices, participants, votesPerPerson, epsilon, deltaExitCriteria)
     n = 0;
@@ -19,42 +12,43 @@ function [cons_avg, dilu_avg, n] = simulate_voting(numChoices, participants, vot
     deltaCounts = 0;
     prefs = zeros(participants, numChoices);
     choiceVotes = zeros(1,numChoices);
-    
+
     while deltaCounts < deltaExitCriteria
         % generate participant preferences
         for i = 1:participants
             prefs(i,:) = randperm(numChoices);
         end
-    
+
         % Add up their votes
+        choiceVotes = zeros(1,numChoices);
         for i = 1:participants
             for j = 1:votesPerPerson
                 choiceVotes(prefs(i,j)) = choiceVotes(prefs(i,j)) + 1;
             end
         end
-    
+
         % Compute consensus and dilution
         cons = consensus(choiceVotes, participants);
         dilu = dilution(choiceVotes, participants);
-    
+
         % Calculate weighted averages
         cons_avg_new = (cons_avg * n + cons) / (n + 1);
         dilu_avg_new = (dilu_avg * n + dilu) / (n + 1);
-    
+
         delta_cons = abs(cons_avg - cons_avg_new);
         delta_dilu = abs(dilu_avg - dilu_avg_new);
-    
+
         delta = max( delta_cons, delta_dilu );
-    
+
         if delta < epsilon
             deltaCounts = deltaCounts + 1;
         else
             deltaCounts = 0;
         end
-    
+
         cons_avg = cons_avg_new;
         dilu_avg = dilu_avg_new;
-    
+
         % increment the loop counter
         n = n + 1;
 
@@ -75,6 +69,7 @@ function [c] = consensus(choiceVotes, participants)
     end
 
     c = c / length(choiceVotes);
+    % fprintf('C = %f\n', c);
 end
 
 function [d] = dilution(choiceVotes, participants)
@@ -88,5 +83,13 @@ function [d] = dilution(choiceVotes, participants)
 
     d = d / length(choiceVotes);
 end
+
+
+
+[c, d, n] = simulate_voting( numChoices, participants, votesPerPerson, epsilon, deltaExitCriteria);
+
+fprintf("Consensus, mean: %f\n", c);
+fprintf("Dilution, mean: %f\n", d);
+fprintf("Number of simulations: %d\n", n);
 
 
