c2da7a74c1d1cf97149e8545d766148057e8ab6c
Paul <pwmohr@gmail.com>
2024-04-08 08:07:17 -0700

    Renamed some terms. Added multi-grouping preference-generation.

--- consensus_dilution.m
+++ consensus_dilution.m
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % PARAMETERS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-epsilon = 0.01;
-numChoices = 100;
-participants = [5:5:50];
-votesPP = [5:5:50];
+epsilon = 0.001;
+numChoices = 20;
+participants = [4:2:20];
+votesPP = [3:1:10];
 deltaExitCriteria = 10;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -26,19 +26,20 @@ deltaExitCriteria = 10;
 %          dilu_avg: Average computed dilution (0,1)
 %                 n: Number of iterations required to achieve exit condition
 %
-function [cons_avg, dilu_avg, n] = simulate_voting(numChoices, participants, votesPP, epsilon, deltaExitCriteria)
+function [cons_avg, satu_avg, reje_avg, n] = simulate_voting(numChoices, participants, votesPP, epsilon, deltaExitCriteria)
     n = 0;
     cons_avg = 0;
-    dilu_avg = 0;
+    satu_avg = 0;
+    reje_avg = 0;
     deltaCounts = 0;
     prefs = zeros(participants, numChoices);
     choiceVotes = zeros(1,numChoices);
     sw='\\\\\\\\||||||||////////--------';
 
-while deltaCounts < deltaExitCriteria
+    while deltaCounts < deltaExitCriteria
         % generate participant preferences
         for i = 1:participants
-            prefs(i,:) = randperm(numChoices);
+            prefs(i,:) = generate_prefs(i, numChoices);
         end
 
         % Add up their votes
@@ -49,18 +50,20 @@ while deltaCounts < deltaExitCriteria
             end
         end
 
-        % Compute consensus and dilution
+        % Compute consensus, dilution, rejected
         cons = consensus(choiceVotes, participants);
-        dilu = dilution(choiceVotes, participants);
+        satu = saturation(choiceVotes, participants);
+        reje = rejected(choiceVotes, participants);
 
         % Calculate weighted averages
         cons_avg_new = (cons_avg * n + cons) / (n + 1);
-        dilu_avg_new = (dilu_avg * n + dilu) / (n + 1);
+        satu_avg_new = (satu_avg * n + satu) / (n + 1);
+        reje_avg_new = (reje_avg * n + reje) / (n + 1);
 
         delta_cons = abs(cons_avg - cons_avg_new);
-        delta_dilu = abs(dilu_avg - dilu_avg_new);
+        delta_satu = abs(satu_avg - satu_avg_new);
 
-        delta = max( delta_cons, delta_dilu );
+        delta = max( delta_cons, delta_satu );
 
         if delta < epsilon
             deltaCounts = deltaCounts + 1;
@@ -69,7 +72,8 @@ while deltaCounts < deltaExitCriteria
         end
 
         cons_avg = cons_avg_new;
-        dilu_avg = dilu_avg_new;
+        satu_avg = satu_avg_new;
+        reje_avg = reje_avg_new;
 
         % increment the loop counter
         n = n + 1;
@@ -90,9 +94,47 @@ end
 %
 % Dilution
 %
-function [d] = dilution(choiceVotes, participants)
+function [d] = saturation(choiceVotes, participants)
     frac = 1.0;
     d = sum(choiceVotes >= frac * participants) / length(choiceVotes);
+end
+
+%
+% Spread
+%
+function [s] = rejected(choiceVotes, participants)
+    frac = 0.1;
+    s = sum(choiceVotes <= frac * participants) / length(choiceVotes);
+end
+
+%
+% Generate Participant Prefs
+%
+function [p] = generate_prefs(x, n)
+    type = 'random-4-groups';
+    switch type
+        case 'random'
+            p = randperm(n);
+        case 'linear'
+            p = 1:n;
+        case 'random-4-groups'
+            p = random4g(n);
+        otherwise
+            error('Unsupported option for generate_prefs().');
+    endswitch
+end
+
+%
+% Randomize preferences in 4 equal groups 1-25, 26-50, etc.
+%
+function [p] = random4g(n)
+    boundary_values = [ 0 0.25 0.5 0.75 1.0 ];
+
+    % Determine group boundary indices
+    b = round(n * [boundary_values]);
+    for i = 2:length(boundary_values)
+        p(b(i-1)+1:b(i)) = randperm(b(i)-b(i-1)) + b(i-1);
+    endfor
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -102,9 +144,7 @@ end
 %
 % Allocate Memory
 %
-c = zeros(length(participants), length(votesPP));
-d = zeros(length(participants), length(votesPP));
-n = zeros(length(participants), length(votesPP));
+c = s = r = n = zeros(length(participants), length(votesPP));
 
 %
 % Run the simulations
@@ -112,8 +152,8 @@ n = zeros(length(participants), length(votesPP));
 for i = 1:length(participants)
   for j = 1:length(votesPP)
     fprintf('Simulating %2d participants %2d votes: ', participants(i), votesPP(j));
-    [c(i,j), d(i,j), n(i,j)] = simulate_voting( numChoices, participants(i), votesPP(j), epsilon, deltaExitCriteria);
-    fprintf('\b  [c = %f, d = %e, n = %d]\n', c(i,j), d(i,j), n(i,j));
+    [c(i,j), s(i,j), r(i,j), n(i,j)] = simulate_voting( numChoices, participants(i), votesPP(j), epsilon, deltaExitCriteria);
+    fprintf('\b  [c = %f, s = %e, r = %f, n = %d]\n', c(i,j), s(i,j), r(i,j), n(i,j));
   end
 end
 
@@ -130,8 +170,16 @@ zlabel("Fraction", "FontSize", 14);
 
 figure;
 [x,y] = meshgrid(participants, votesPP);
-mesh(x, y, d');
-title("Dilution", "FontSize", 20);
+mesh(x, y, s');
+title("Saturated", "FontSize", 20);
+xlabel("# of Participants", "FontSize", 14);
+ylabel("# of Votes per Participant", "FontSize", 14);
+zlabel("Fraction", "FontSize", 14);
+
+figure;
+[x,y] = meshgrid(participants, votesPP);
+mesh(x, y, r');
+title("Rejected", "FontSize", 20);
 xlabel("# of Participants", "FontSize", 14);
 ylabel("# of Votes per Participant", "FontSize", 14);
 zlabel("Fraction", "FontSize", 14);
